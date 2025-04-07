e480b3508a8d876cd422b888d00ba722da3681a1
Strokkur424 <133226102+Strokkur424@users.noreply.github.com>
2025-04-07 15:30:26 +0200

    Add 1.21.3, 1.21.2, and 1.21.1 changelogs

--- astro.config.mjs
+++ astro.config.mjs
@@ -54,7 +54,12 @@ export default defineConfig({
         },
         {
           label: "API Changelogs",
-          autogenerate: { directory: "changelogs" },
+          items: [
+            { label: "1.21.4", link: "/changelogs/1.21.4" },
+            { label: "1.21.3", link: "/changelogs/1.21.3" },
+            { label: "1.21.2", link: "/changelogs/1.21.2" },
+            { label: "1.21.1", link: "/changelogs/1.21.1" },
+          ],
         },
       ],
       customCss: ["./src/custom.css"],
