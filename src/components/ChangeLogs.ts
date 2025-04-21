28c9d0a62eb36a0cd6ae1bcbffa20e2d8407fb1b
Strokkur24 <133226102+Strokkur424@users.noreply.github.com>
2025-04-21 18:26:41 +0200

    Add API Changelogs (#8)
    * Add 1.21.4 api additions
    
    * Add 1.21.3, 1.21.2, and 1.21.1 changelogs
    
    * Add API Additions overview to 1.21.1
    
    * Fix spelling
    
    * Add latest build
    
    * I made a (datadriven) mess!
    
    * Stupid autoimport
    
    * Convert 1.21.3 to data driven
    
    * Fixup compile errors
    
    * Add 'marked' dependency
    
    * Move to pages and separate render from data
    
    * Remove unused files
    
    * Update title
    
    * Fix table of contents
    
    * Add 1.21.5 change logs
    
    * Revert "Merge branch 'main' into feat/api-changes"
    
    This reverts commit ae4e70aac150b9d145946e66393a5e40d84fd5a9, reversing
    changes made to 13485d68d5bfa9fe72023df17551f2cffb57a863.
    
    * Reapply "Merge branch 'main' into feat/api-changes"
    
    This reverts commit 864149a1e4751f96cc6b1530bd1cbe649bf4139e.
    
    * Fix all merge issues :/
    
    * Migrate 1.21.4

---
+++ src/components/ChangeLogs.ts
@@ -0,0 +1,123 @@
+import V_1_21_1 from "../assets/changelogs/1_21_1.json";
+import V_1_21_3 from "../assets/changelogs/1_21_3.json";
+import V_1_21_4 from "../assets/changelogs/1_21_4.json";
+import V_1_21_5 from "../assets/changelogs/1_21_5.json";
+
+const versionConversion: Map<string, any> = new Map(
+  Object.entries({
+    "1.21.1": V_1_21_1,
+    "1.21.3": V_1_21_3,
+    "1.21.4": V_1_21_4,
+    "1.21.5": V_1_21_5,
+  }),
+);
+
+export interface SidebarsProps {
+  depth: number;
+  slug: string;
+  text: string;
+}
+
+const cachedChangeLogs: Map<string, ChangeLogs> = new Map();
+export function getChangeLogs(version: string): ChangeLogs {
+  let out: ChangeLogs | undefined = cachedChangeLogs.get(version);
+  if (out != undefined) {
+    return out;
+  }
+
+  return new ChangeLogs(version);
+}
+
+export interface VersionTemplate {
+  name: string;
+  link: string;
+  id: number;
+
+  extra?: string;
+  additions?: string[];
+  deprecations?: string[];
+  extensions?: string[];
+  removed?: string[];
+  obsolete?: string[];
+  renamed?: string[];
+  noInternal?: string[];
+  libraries?: string[];
+}
+
+function flattenValues(
+  change: ChangeLogs,
+  inter: (ver: VersionTemplate) => Array<string> | null,
+): Array<string> {
+  let out: Array<string> = new Array<string>();
+  change.data
+    .map((ver) => inter(ver))
+    .filter((val) => val != null)
+    .forEach((val) => val.forEach((sub) => out.push(sub)));
+  return out;
+}
+
+function createHeadingObj(build: VersionTemplate): SidebarsProps {
+  return {
+    depth: 2,
+    slug: build.id.toString(),
+    text: build.name,
+  };
+}
+
+export class ChangeLogs {
+  version: string;
+  data: VersionTemplate[];
+
+  flatAdditions: string[];
+  flatDeprecations: string[];
+  flatExtensions: string[];
+  flatRemoved: string[];
+  flatObsolete: string[];
+  flatRenamed: string[];
+  flatNoInternal: string[];
+  flatLibraries: string[];
+
+  headings: SidebarsProps[];
+  staticPaths: string[];
+
+  constructor(version: string) {
+    this.version = version;
+
+    let temp: any | undefined = versionConversion.get(version);
+    if (temp == undefined) {
+      throw "No valid version was given!";
+    }
+
+    this.data = temp as Array<VersionTemplate>;
+
+    this.flatAdditions = flattenValues(this, (ver) =>
+      ver.additions == undefined ? null : ver.additions,
+    );
+    this.flatDeprecations = flattenValues(this, (ver) =>
+      ver.deprecations == undefined ? null : ver.deprecations,
+    );
+    this.flatExtensions = flattenValues(this, (ver) =>
+      ver.extensions == undefined ? null : ver.extensions,
+    );
+    this.flatRemoved = flattenValues(this, (ver) =>
+      ver.removed == undefined ? null : ver.removed,
+    );
+    this.flatObsolete = flattenValues(this, (ver) =>
+      ver.obsolete == undefined ? null : ver.obsolete,
+    );
+    this.flatRenamed = flattenValues(this, (ver) =>
+      ver.renamed == undefined ? null : ver.renamed,
+    );
+    this.flatNoInternal = flattenValues(this, (ver) =>
+      ver.noInternal == undefined ? null : ver.noInternal,
+    );
+    this.flatLibraries = flattenValues(this, (ver) =>
+      ver.libraries == undefined ? null : ver.libraries,
+    );
+
+    this.headings = this.data.map((e) => createHeadingObj(e));
+    this.staticPaths = this.headings.map((e) => e.slug);
+
+    cachedChangeLogs.set(version, this);
+  }
+}
